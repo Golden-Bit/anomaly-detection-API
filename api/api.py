9a6c12e026dcb7df3a5b0781654541403809410b
sans <simonesansalone777@gmail.com>
2024-07-30 02:08:37 +0200

    first commit to upload project

---
+++ api/api.py
@@ -0,0 +1,153 @@
+from typing import Optional, Union
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+from pathlib import Path
+from utilities.utilities import infer_wf, test_wf, validate_model, train_model, create_folder_datasets, \
+    visualize_data, setup_dataset, export_model
+
+app = FastAPI(
+    title="Anomaly Detection API",
+    description="API for training, testing, and inference using Padim model"
+)
+
+
+class TrainRequest(BaseModel):
+    dataset_root: str = Field(..., example="datasets/hazelnut_toy", description="Root directory of the dataset")
+    normal_dir: str = Field(..., example="good", description="Directory containing normal images")
+    abnormal_dir: str = Field(..., example="crack", description="Directory containing abnormal images")
+    mask_dir: str = Field(..., example="mask", description="Directory containing masks for abnormal images")
+    image_size: tuple[int, int] = Field((256, 256), example=(256, 256),
+                                        description="Size to which images should be resized")
+
+
+class InferenceRequest(BaseModel):
+    image_path: str = Field(..., example="datasets/hazelnut_toy/crack/01.jpg",
+                            description="Path to the image for inference")
+    root_dir: Optional[str] = Field(None, example="results/Padim/hazelnut_toy/latest",
+                                    description="Root directory for inference")
+    openvino_model_path: Optional[str] = Field(None,
+                                               example="results/Padim/hazelnut_toy/latest/weights/openvino/model.bin",
+                                               description="Path to the OpenVINO model")
+    metadata: Optional[str] = Field(None, example="results/Padim/hazelnut_toy/latest/weights/openvino/metadata.json",
+                                    description="Path to the metadata file for the OpenVINO model")
+
+
+class TestRequest(BaseModel):
+    dataset_root: str = Field(..., example="datasets/hazelnut_toy", description="Root directory of the dataset")
+    normal_dir: str = Field(..., example="good", description="Directory containing normal images")
+    abnormal_dir: str = Field(..., example="crack", description="Directory containing abnormal images")
+    mask_dir: str = Field(..., example="mask", description="Directory containing masks for abnormal images")
+    ckpt_path: str = Field(..., example="results/Padim/hazelnut_toy/v2/weights/lightning/model.ckpt",
+                           description="Path to the model checkpoint")
+
+
+@app.post("/train", summary="Train the model", description="Train the Padim model using the provided dataset")
+def train(request: TrainRequest):
+    """
+    This endpoint trains the Padim model using the provided dataset.
+
+    Parameters:
+    - request: TrainRequest
+        - dataset_root: Root directory of the dataset.
+        - normal_dir: Directory containing normal images.
+        - abnormal_dir: Directory containing abnormal images.
+        - mask_dir: Directory containing masks for abnormal images.
+        - image_size: Size to which images should be resized.
+
+    Returns:
+    - JSON response with status of the training process.
+    """
+    dataset_root = Path.cwd() / Path(request.dataset_root)
+    normal_dir = request.normal_dir
+    abnormal_dir = request.abnormal_dir
+    mask_dir = request.mask_dir
+    image_size = request.image_size
+
+    try:
+        dataset_root = Path(dataset_root)
+        folder_datamodule = setup_dataset(dataset_root, normal_dir, abnormal_dir, mask_dir)
+        visualize_data(folder_datamodule)
+
+        create_folder_datasets(dataset_root, normal_dir, abnormal_dir, mask_dir, image_size)
+
+        engine, model = train_model(folder_datamodule)
+
+        validate_model(engine, model, folder_datamodule, engine.best_model_path)
+
+        export_model(engine, model)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"status": "Training completed successfully"}
+
+
+@app.post("/inference", summary="Perform inference", description="Perform inference using the trained model")
+def inference(request: InferenceRequest):
+    """
+    This endpoint performs inference using the trained Padim model.
+
+    Parameters:
+    - request: InferenceRequest
+        - image_path: Path to the image for inference.
+        - root_dir: Root directory for inference (optional).
+        - openvino_model_path: Path to the OpenVINO model (optional).
+        - metadata: Path to the metadata file for the OpenVINO model (optional).
+
+    Returns:
+    - JSON response with status of the inference process and the results.
+    """
+    image_path = request.image_path
+    root_dir = Path.cwd() / Path(request.root_dir)
+    openvino_model_path = request.openvino_model_path
+    metadata = request.metadata
+
+    try:
+        response = infer_wf(
+            engine=None,
+            image_path=image_path,
+            root_dir=root_dir,
+            openvino_model_path=openvino_model_path,
+            metadata=metadata
+        )
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"status": "Inference completed successfully"}#, "results": response.__dict__}
+
+
+@app.post("/test", summary="Test the model", description="Test the model using the provided dataset and checkpoint")
+def test(request: TestRequest):
+    """
+    This endpoint tests the Padim model using the provided dataset and checkpoint.
+
+    Parameters:
+    - request: TestRequest
+        - dataset_root: Root directory of the dataset.
+        - normal_dir: Directory containing normal images.
+        - abnormal_dir: Directory containing abnormal images.
+        - mask_dir: Directory containing masks for abnormal images.
+        - ckpt_path: Path to the model checkpoint.
+
+    Returns:
+    - JSON response with status of the testing process.
+    """
+    dataset_root = Path.cwd() / Path(request.dataset_root)
+    normal_dir = request.normal_dir
+    abnormal_dir = request.abnormal_dir
+    mask_dir = request.mask_dir
+    ckpt_path = request.ckpt_path
+
+    try:
+        test_wf(dataset_root, normal_dir, abnormal_dir, mask_dir, ckpt_path)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+    return {"status": "Testing completed successfully"}
+
+
+if __name__ == "__main__":
+
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8001)
+
---
+++ api/utilities/utilities.py
@@ -0,0 +1,152 @@
+from typing import Optional, Union
+from pathlib import Path
+import numpy as np
+from PIL import Image
+from torchvision.transforms.v2 import Resize
+from torchvision.transforms.v2.functional import to_pil_image
+from anomalib.data.image.folder import Folder, FolderDataset
+from anomalib import TaskType
+from anomalib.models import Padim
+from anomalib.engine import Engine
+from anomalib.deploy import ExportType, OpenVINOInferencer
+from matplotlib import pyplot as plt
+
+
+def setup_dataset(dataset_root: Path, normal_dir: str, abnormal_dir: str, mask_dir: str) -> Folder:
+    folder_datamodule = Folder(
+        name="hazelnut_toy",
+        root=dataset_root,
+        normal_dir=normal_dir,
+        abnormal_dir=abnormal_dir,
+        task=TaskType.SEGMENTATION,
+        mask_dir=dataset_root / mask_dir / abnormal_dir,
+        image_size=(256, 256),
+        num_workers=0
+    )
+    folder_datamodule.setup()
+    return folder_datamodule
+
+
+def visualize_data(folder_datamodule: Folder):
+    i, data = next(enumerate(folder_datamodule.train_dataloader()))
+    print(data.keys(), data["image"].shape)
+
+    img = to_pil_image(data["image"][0].clone())
+    msk = to_pil_image(data["mask"][0]).convert("RGB")
+    Image.fromarray(np.hstack((np.array(img), np.array(msk)))).show()
+
+
+def create_folder_datasets(dataset_root: Path, normal_dir: str, abnormal_dir: str, mask_dir: str,
+                           image_size: tuple) -> tuple:
+    transform = Resize(image_size, antialias=True)
+
+    folder_dataset_segmentation_train = FolderDataset(
+        name="hazelnut_toy",
+        normal_dir=dataset_root / normal_dir,
+        abnormal_dir=dataset_root / abnormal_dir,
+        split="train",
+        transform=transform,
+        mask_dir=dataset_root / mask_dir / abnormal_dir,
+        task=TaskType.SEGMENTATION,
+    )
+
+    folder_dataset_segmentation_test = FolderDataset(
+        name="hazelnut_toy",
+        normal_dir=dataset_root / normal_dir,
+        abnormal_dir=dataset_root / abnormal_dir,
+        split="test",
+        transform=transform,
+        mask_dir=dataset_root / mask_dir / abnormal_dir,
+        task=TaskType.SEGMENTATION,
+    )
+
+    return folder_dataset_segmentation_train, folder_dataset_segmentation_test
+
+
+def train_model(folder_datamodule: Folder):
+    model = Padim()
+    engine = Engine(task=TaskType.SEGMENTATION)
+    engine.fit(model=model, datamodule=folder_datamodule)
+    return engine, model
+
+
+def validate_model(engine: Engine, model: Padim, folder_datamodule: Folder, ckpt_path: str):
+    test_results = engine.test(
+        model=model,
+        datamodule=folder_datamodule,
+        ckpt_path=ckpt_path
+    )
+    print(test_results)
+    return test_results
+
+
+def export_model(engine: Engine, model: Padim):
+    engine.export(model=model, export_type=ExportType.OPENVINO)
+
+
+def perform_inference(engine: Optional[Engine],
+                      image_path: Optional[Union[str, Path]],
+                      root_dir: Optional[Union[str, Path]],
+                      openvino_model_path: Optional[Union[str, Path]],
+                      metadata: Optional[Union[str, Path]]):
+    if engine:
+        root_dir = Path(engine.trainer.default_root_dir)
+
+    if not openvino_model_path:
+        openvino_model_path = root_dir / "weights" / "openvino" / "model.bin"
+
+    if not metadata:
+        metadata = root_dir / "weights" / "openvino" / "metadata.json"
+
+    assert openvino_model_path.exists()
+    assert metadata.exists()
+
+    inferencer = OpenVINOInferencer(
+        path=openvino_model_path,
+        metadata=metadata,
+        device="CPU",
+    )
+
+    predictions = inferencer.predict(image=image_path)
+
+    print(predictions.pred_score, predictions.pred_label)
+    plt.imshow(predictions.image)
+    plt.imshow(predictions.anomaly_map)
+    plt.imshow(predictions.heat_map)
+    plt.imshow(predictions.pred_mask)
+    plt.imshow(predictions.segmentations)
+
+    return predictions
+
+
+def infer_wf(engine: Optional[Engine],
+             image_path: Optional[Union[str, Path]],
+             root_dir: Optional[Union[str, Path]],
+             openvino_model_path: Optional[Union[str, Path]] = None,
+             metadata: Optional[Union[str, Path]] = None):
+    inference_response = perform_inference(engine=engine,
+                                           image_path=Path(image_path),
+                                           root_dir=root_dir,
+                                           openvino_model_path=openvino_model_path,
+                                           metadata=metadata)
+
+    return inference_response
+
+
+def test_wf(dataset_root: Optional[Union[str, Path]],
+            normal_dir: Optional[Union[str, Path]],
+            abnormal_dir: Optional[Union[str, Path]],
+            mask_dir: Optional[Union[str, Path]],
+            ckpt_path: Optional[Union[str, Path]]):
+    dataset_root = Path(dataset_root)
+    folder_datamodule = setup_dataset(dataset_root, normal_dir, abnormal_dir, mask_dir)
+    model = Padim()
+    engine = Engine(task=TaskType.SEGMENTATION)
+    test_response = validate_model(engine, model, folder_datamodule, ckpt_path)
+    print(test_response)
+
+
+if __name__ == "__main__":
+
+    pass
+
